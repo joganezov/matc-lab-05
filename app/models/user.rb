7bdfb5a7dc718613e7d233d9465bd642a00581c4
Jeff Oganezov <joganezov@madisoncollege.edu>
2015-03-05 23:30:19 +0000

    final

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase, self.name = name.downcase }
+  validates :name,  presence: true, length: { minimum: 3 }, 
+                uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
